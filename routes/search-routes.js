52efaedc360548fd77d7891db5b7c0198c3f1383
Mahmoud Badran <mahmoudbd@gmail.com>
2020-07-01 14:35:06 +0200

    add backend

--- routes/search-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const searchController = require("../controllers/search-controllers");
-
-const router = express.Router();
-
-router.get("/search", searchController.searchUsersPlaces);
-
-module.exports = router;
